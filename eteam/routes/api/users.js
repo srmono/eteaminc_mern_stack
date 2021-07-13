27098ef99e6543f5e9dddb69002b60c5e3f7e665
srmono <bvsrao91@gmail.com>
2021-07-13 23:22:09 +0530

    Start with Project

---
+++ eteam/routes/api/users.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator');
+
+const User = require('../../models/User');
+
+//@route    POST api/users
+//@desc     Regiser User
+//@access   Public
+router.post( '/',
+ [
+     check('name', "Name is Required").not().isEmpty(),
+     check('email', "Please enter valid email").isEmail(),
+     check('password', "Ener Min 6 charaters").isLength({min: 6})
+ ],
+ async (req, res) => {
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() })
+    }
+
+    const { name , email, password } = req.body;
+
+    try {
+
+        let user = await User.findOne({email })
+
+        if (user) {
+          res.status(400).json({ errors: [{ msg: "User Already Exists " }] });
+        }
+
+    const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm'
+    })
+
+    user = new User({
+        name,
+        email,
+        avatar,
+        password
+    })
+
+    const salt = await bcrypt.genSalt(10);
+
+    user.password = await bcrypt.hash(password, salt)
+
+    await user.save()
+
+    // Return JsonWebToken
+
+
+
+    res.send("User Route")
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).send(" Server Error")
+    }    
+})
+
+module.exports = router;
+
---
+++ eteam/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const connectDB = require("./config/db");
+const app = express();
+
+//Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.urlencoded()); // Parse URL-encoded bodies
+app.use(express.json());// User to parese JSON bodies
+
+app.get("/", (req, res) => res.send("Eteam App is running") );
+
+// Define Routes
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+
+
+
+

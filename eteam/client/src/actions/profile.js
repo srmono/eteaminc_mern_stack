4483da6108ff377a68a2643815ba703e108cfd56
srmono <bvsrao91@gmail.com>
2021-07-25 08:44:45 +0530

    profile issue updated

--- eteam/client/src/actions/profile.js
+++ eteam/client/src/actions/profile.js
@@ -6,23 +6,6 @@ import {
     PROFILE_ERROR
 } from './types'
 
-// // Get current users profile
-// export const getCurrentProfile = () => async dispatch =>  {
-//     try {
-//         const res = axios.get("http://localhost:5000/api/profile/me");
-
-//         dispatch({
-//             type: GET_PROFILE,
-//             payload: res.data 
-//         })
-//     } catch (err) {
-//         dispatch({
-//             type: PROFILE_ERROR,
-//             payload: { msg: err.response.statusText, status: err.response.status}
-//         })
-//     }
-// }
-
 //Get Current user Profile
 export const getCurrentProfile = () => async dispatch => {
     try {
@@ -31,12 +14,12 @@ export const getCurrentProfile = () => async dispatch => {
             type: GET_PROFILE,
             payload: res.data
         })
-    } catch (error) {
+    } catch (err) {
         dispatch({
           type: PROFILE_ERROR,
           payload: {
-            msg: error.response.statusText,
-            status: error.response.status,
+            msg: err.response.statusText,
+            status: err.response.status,
           },
         });
     }
